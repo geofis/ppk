e6cdaedba28dac79c420983820579758ed3cae3c
geofis <joseramon@geografiafisica.org>
2021-08-15 10:14:54 -0400

    Better root name strategy for output files in solution

--- solutions.sh
+++ solutions.sh
@@ -3,8 +3,8 @@
   if [[ $# -ne 6 ]]; then
     echo ""
     echo " Usage: solutions.sh solution_type conf_file_path base_position_type basepos1 basepos2 basepos3"
-    echo " solution_type          (1:fix,2:float) [1]"
-    echo " base_position_type     (l:latitude/longitude/height,r:ecef pos in m) [l]"
+    echo " solution_type          (1:fix,2:float)"
+    echo " base_position_type     (l:latitude/longitude/height,r:ecef pos in m)"
     echo ""
     exit 2
   fi
@@ -18,7 +18,7 @@ basepos3=$6
 
 # Convert RTK solutions to KML and extract observations from UBX to RINEX v 3.03 format
 cd rover
-for i in *.ubx; do pos2kml -q $soltype -o ${i/ubx*.ubx/rtk.kml} $i; convbin -v 3.03 -os -o ${i/-ubx*.ubx/.obs} $i; done
+for i in *.ubx; do root_name=`echo $i | sed -E "s/(-[a-z])*([.a-z]*)//g"`; pos2kml -q $soltype -o $root_name-rtk.kml $i; convbin -v 3.03 -os -o $root_name.obs $i; done
 cd ..
 
 # Generate PPK solutions
--- unzip_merge_rinex.sh
+++ unzip_merge_rinex.sh
@@ -4,7 +4,7 @@ year=$1
 
 # Rinex v 2.11
 for i in base/rinex_v211/*.zip; do unzip $i -d base/rinex_v211 *${year}O; done
-./teqc -phc base/rinex_v211/*${year}O > base/rinex_v211/merged.obs
+teqc -phc base/rinex_v211/*${year}O > base/rinex_v211/merged.obs
 rm base/rinex_v211/*${year}O
 
 # Rinex v 3.03
